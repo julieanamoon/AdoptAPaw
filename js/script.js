566aad2a060d1620c4ee9562f1ebe18ca78ff74c
julieanamoon <julieanamoon@hotmail.com>
2021-03-29 23:24:56 -0400

    update
    fixes

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function start(){
 }
 
 
-$(document).on("click", "nav a[href]", nav);
+$(document).on("click", "#nav a[href]", nav);
 
 function nav(e){
 
@@ -18,4 +18,40 @@ function nav(e){
 
     $("#nav a").fadeTo("fast", 0.25);
     $(this).fadeTo("fast", 1);
+
+
+    $("#nav_left").fadeTo(0, 0);
+
+    if (href == "home.html") {
+        $("#nav_title").text("Adopt A Paw");
+    } else if (href== "search.html") {
+        $("#nav_title").text("Search");
+    } else if (href== "match.html") {
+        $("#nav_title").text("Your Matches");
+    }else if (href== "favs.html") {
+        $("#nav_title").text("Your Favourites");
+    } 
 }
+
+
+// $(document).on("click", "#content a[href]", contentNav);
+
+// function contentNav(e){
+
+//     e.preventDefault()
+//     // e represents event and we can prevent the default behaviour of the specific hyperlink
+//     var href = $(this).attr("href");
+
+//     $("#content").load("modules/" + href);
+
+//     $("#nav_left").fadeTo(0, 1);
+// }
+
+
+
+
+
+$(document).ready(function() {
+
+    $.ajaxSetup({ cache: false });
+  });
