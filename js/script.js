da181bb77e5b67fe05bd4dbfe9180e55c9004d73
julieanamoon <julieanamoon@hotmail.com>
2021-04-19 00:14:00 -0400

    update

--- js/script.js
+++ js/script.js
@@ -51,8 +51,63 @@ function contentNav(e){
 
 
 
+
+// SEARCH CODE
+function place(index, obj){
+    $("#list").append("<img class='d-block w-100 mb-2' src='"+obj.thumbnail+"'>");
+    }
+    
+    function populate(data){
+    $("#list").empty();
+    $.each(data.results , place);
+
+
+    
+    }
+    
+    function search (e) {
+    
+        if (e.which == 13) {
+    
+        var keyword = $("#keyword").val();
+    
+        if (keyword.length < 4) {
+        alert("Your keyword is too short!");
+       
+       } else {
+    
+        var url = "https://api.creativecommons.engineering/v1/images?format=json&q="+keyword;
+    
+        $.ajax({
+        url: url,
+        success: populate
+
+    });
+    
+    }
+    }
+    
+    }
+    
+    $(document).on("keyup", "#keyword", search);
+
+
+
+
+
+
+
+
+
+
+
+
+// CLEAR CACHE
 $(document).ready(function() {
 
     $.ajaxSetup({ cache: false });
     
   });
+
+
+
